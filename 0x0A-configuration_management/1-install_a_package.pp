de079992e404385c3c04f527d840df62e16181d7
Freedom-21 <yenesewfreedom8@gmail.com>
2024-06-30 20:54:27 +0200

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # installing flask framework via Puppet
-package {'Flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
